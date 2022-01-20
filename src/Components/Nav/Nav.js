c59c9d3caa8c5f5bd785e7a6dda5df0b902fce20
Kirill Galenko <kirill.galenko@awave.com>
2022-01-20 15:57:34 +0200

    top nav!

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,6 +1,7 @@
 import {Icon} from "../IcoMoon/Icon";
 import {useNavigate} from "react-router-dom";
 import Dropdown from "react-bootstrap/Dropdown";
+import dailyDoLogo from "../../img/dailyDo.svg";
 
 export const Nav = (props) => {
     const navigate = useNavigate();
@@ -9,13 +10,19 @@ export const Nav = (props) => {
         props.logOut(e);
     }
     return(
-        <nav>
+        <nav className="main-nav">
+            <a className="dailydo-logo" href="/">
+                <img src={dailyDoLogo} alt="dailyDo" />
+            </a>
             <Dropdown>
                 <Dropdown.Toggle variant="link" id="dropdown-basic" aria-label="Menu toggle button">
-                    <Icon color='#444' size="14px" icon='menu' />
+                    <Icon color='#0d82df' size="14px" icon='menu' />
                 </Dropdown.Toggle>
 
                 <Dropdown.Menu>
+                    <Dropdown.Item href="/">Today</Dropdown.Item>
+                    <Dropdown.Item href="#/action-1">Past todos</Dropdown.Item>
+                    <Dropdown.Divider />
                     <Dropdown.Item href="#/action-1">Profile</Dropdown.Item>
                     <Dropdown.Divider />
                     <Dropdown.Item onClick={handleLogoutClick}><Icon color='#444' size="12px" icon='log-out' /> Log out</Dropdown.Item>
