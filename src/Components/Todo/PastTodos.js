3378999f9d4adb39d4b7a28c5635f10d3ccb662e
Kirill Galenko <kirill.galenko@awave.com>
2022-01-21 17:01:50 +0200

    added test ajax to fetch all todos

--- src/Components/Todo/PastTodos.js
+++ src/Components/Todo/PastTodos.js
@@ -1,5 +1,25 @@
+import React, { useEffect } from 'react';
 
 export const PastTodos = (props) => {
+    useEffect(() => {
+        const authUrl = process.env.REACT_APP_API_DOMAIN + '/wp-json/ddapi/todo/get/000';
+        const token = localStorage.getItem('token');
+                
+        fetch(authUrl, {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer' + token
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            if(data !== 404){
+                const parsed = JSON.parse(data);
+                // this.setState({todos: parsed });
+                console.log(parsed);
+            }
+        });
+    });
     return(
         <h1>Past todos list</h1>
     );
