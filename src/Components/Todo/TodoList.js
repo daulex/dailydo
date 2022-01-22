831355c607bffbd9e1bc83810bf5c2da6e7afdbb
Kirill Galenko <kirill.galenko@awave.com>
2022-01-22 18:48:54 +0200

    rewrote todolist.js into a function component

--- src/Components/Todo/TodoList.js
+++ src/Components/Todo/TodoList.js
@@ -1,23 +1,17 @@
-import React from 'react';
-import { TodoItem } from './TodoItem';
-// import {todosData} from '../Data/todosData';
+import React, { useState, useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import {array_move} from '../../utilities';
+import { TodoItem } from './TodoItem';
+
+export const TodoList = (props) => {
+    const [ initialLoad, setInitialLoad ] = useState(false);
+    const [ syncRequired, setSyncRequired ] = useState(false);
+    const [ todos, setTodos ] = useState([]);
 
 
-export class TodoList extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = { todos: [] };
-        
-        this.handleTaskCreate = this.handleTaskCreate.bind(this);
-        this.handleTaskDelete = this.handleTaskDelete.bind(this);
-        this.syncStateToDB = this.syncStateToDB.bind(this);
-        this.handleTaskMove = this.handleTaskMove.bind(this);
-        this.handleTaskEdit = this.handleTaskEdit.bind(this);
-    }
-    syncStateToDB = () => {
+    const syncStateToDB = () => {
+        console.log('sync triggered');
         const authUrl = process.env.REACT_APP_API_DOMAIN + '/wp-json/ddapi/update-todo';
         const token = localStorage.getItem('token');
         
@@ -26,17 +20,17 @@ export class TodoList extends React.Component{
             headers: {
                 'Authorization': 'Bearer' + token
             },
-            body: JSON.stringify(this.state)
+            body: JSON.stringify({todos: todos})
         }).then(response => {
             if(response.status !== 200){
                 console.log('Something went wrong');
             }
         });
     };
-    handleChange = id => {
-        const { todos } = this.state;
-        this.setState({
-            todos: todos.map(todo => {
+    const handleChange = id => {
+        
+        setTodos(
+            todos.map(todo => {
                 if(todo.id === id){
                     return {
                         ...todo,
@@ -45,12 +39,12 @@ export class TodoList extends React.Component{
                 }
                 return todo;
             })
-        }, this.syncStateToDB);
+        );
+        setSyncRequired(true);
     }
-    handleTaskEdit = (id, newText) => {
-        const { todos } = this.state;
-        this.setState({
-            todos: todos.map(todo => {
+    const handleTaskEdit = (id, newText) => {
+        
+        setTodos(todos.map(todo => {
                 if(todo.id === id){
                     return {
                         ...todo,
@@ -58,16 +52,17 @@ export class TodoList extends React.Component{
                     };
                 }
                 return todo;
-            })
-        }, this.syncStateToDB);
+            }) );
+
+        setSyncRequired(true);
     }
-    handleTaskCreate = e => {
+    const handleTaskCreate = e => {
         e.preventDefault();
         let val = e.target.elements[0].value;
         if(val.length){
             e.target.elements[0].value = '';
             
-            const ids = this.state.todos.map(object => {
+            const ids = todos.map(object => {
                 return object.id;
             });
             
@@ -76,91 +71,105 @@ export class TodoList extends React.Component{
                 nextId = Math.max(...ids) + 1;
             }
             
-            this.setState({ todos: [...this.state.todos, {
+            setTodos( [...todos, {
                 id: nextId,
                 text: val,
                 completed: false
-            }] }, this.syncStateToDB);
+            }] );
+            setSyncRequired(true);
         }
     }
-    handleTaskDelete = x => {
-        this.setState({ todos: this.state.todos.filter(task => {
+    const handleTaskDelete = x => {
+        setTodos( todos.filter(task => {
             return task.id !== x;
-        }) }, this.syncStateToDB);
+        }) );
+        setSyncRequired(true);
     }
-    handleTaskMove = (x, direction) => {
+
+    const handleTaskMove = (x, direction) => {
         
-        const currentIndex = this.state.todos.findIndex(each => each.id === x);
+        const currentIndex = todos.findIndex(each => each.id === x);
         let newIndex = currentIndex;
-        const length = this.state.todos.length;
+        
+        const length = todos.length;
         if(direction === 'up' && currentIndex !== 0){
             newIndex--;
         }else if(direction === 'down' && currentIndex !== length-1){
             newIndex++;
         }
-        this.setState(
-            {todos: array_move(this.state.todos, currentIndex, newIndex)},
-            this.syncStateToDB
-            );
+        array_move(todos, currentIndex, newIndex);
+        
+
+        setTodos( todos );
+        setSyncRequired(true);
             
-        }
-        render(){
-            const todoItemComponents = this.state.todos.map(item => {
-                return <TodoItem
-                key={item.id}
-                item={item}
-                handleChange={this.handleChange}
-                handleTaskDelete={this.handleTaskDelete}
-                handleTaskMove={this.handleTaskMove}
-                handleTaskEdit={this.handleTaskEdit}
-                />
-            });        
-            return(
-                <div className="todos">
-                <div className="todos-items">
-                {todoItemComponents}
-                </div>
-                <Form onSubmit={this.handleTaskCreate} className='new-task-form'>
+    }
+
+    const todoItemComponents = todos.map(item => {
+        return <TodoItem
+        key={item.id}
+        item={item}
+        handleChange={handleChange}
+        handleTaskDelete={handleTaskDelete}
+        handleTaskMove={handleTaskMove}
+        handleTaskEdit={handleTaskEdit}
+        />
+    });   
+
+    useEffect(() => {
+        console.log(syncRequired);
+        if(!initialLoad){
+            let url = process.env.REACT_APP_API_DOMAIN + '/wp-json/ddapi/todo/get';
+            const token = localStorage.getItem('token');
                 
-                <Form.Label htmlFor="inlineFormInputName" visuallyHidden>
-                Describe your new task
-                </Form.Label>
-                <Form.Control name="newTaskText" id="newTaskText" placeholder="Describe your new task" />
-                <Button type="submit">Add</Button>
-                </Form>
-                {this.state.todos.length === 0 && 
-                    <div className="todo-from-template">
-                        <h3>- or -</h3>
-                        <Form.Select aria-label="Todo list from template">
-                            <option>Todo list from template</option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
-                        </Form.Select>
-                    </div>
+            fetch(url, {
+                method: 'GET',
+                headers: {
+                    'Authorization': 'Bearer' + token
                 }
-                </div>
-                );
-            }
+            })
+            .then(response => response.json())
+            .then(data => {
+                if(data !== 404){
+                
+                    setTodos( JSON.parse(data) );
+                }
+            });
+            setInitialLoad(true);
             
-            componentDidMount() {
-                
-                let url = process.env.REACT_APP_API_DOMAIN + '/wp-json/ddapi/todo/get';
-                const token = localStorage.getItem('token');
-                console.log();
-                
-                fetch(url, {
-                    method: 'GET',
-                    headers: {
-                        'Authorization': 'Bearer' + token
-                    }
-                })
-                .then(response => response.json())
-                .then(data => {
-                    if(data !== 404){
-                        const parsed = JSON.parse(data);
-                        this.setState({todos: parsed });
-                    }
-                });
-            }
         }
+        if(syncRequired){
+            syncStateToDB();
+            setSyncRequired(false);
+        }
+        
+    });
+
+    return(
+        <div className="todos">
+        <div className="todos-items">
+        {todoItemComponents}
+        </div>
+        <Form onSubmit={handleTaskCreate} className='new-task-form'>
+                    
+        <Form.Label htmlFor="inlineFormInputName" visuallyHidden>
+        Describe your new task
+        </Form.Label>
+        <Form.Control name="newTaskText" id="newTaskText" placeholder="Describe your new task" />
+        <Button type="submit">Add</Button>
+        </Form>
+        {todos.length === 0 && 
+            <div className="todo-from-template">
+                <h3>- or -</h3>
+                <Form.Select aria-label="Todo list from template">
+                    <option>Todo list from template</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                </Form.Select>
+            </div>
+        }
+        </div>
+        );
+    
+}
