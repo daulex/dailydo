cd69eda3165707c3cfcaede28d4959edee383084
Kirill Galenko <kirill.galenko@awave.com>
2022-02-01 20:47:45 +0200

    fixed imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-export const App = () => {
+export default function App(){
     return(
         <h1>Here we go again!</h1>
     );
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.scss';
-import {App} from './App';
+import App from './App';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import {BrowserRouter, Route, Routes} from "react-router-dom";
 
